1ffc3016c3646706410071e0c0353f2f49914ccc
luka <120731202+poladaa@users.noreply.github.com>
2024-10-16 10:29:26 +0400

    Add files via upload

---
+++ leqcia 3/lecture 3/script.js
@@ -0,0 +1,53 @@
+// console.log('leqcia 3')
+
+// function task(m,n) {
+
+//     if(m<n) {
+//         for(let i = m + 1;i<n; i++ ) {
+//             document.write(`<p>${i}</p>`)
+//           }
+//     } else if {
+//         for(let i = m - 1;i>n; i++ ) {
+//             document.write(`<p>${i}</p>`)
+//           }
+//     }else{
+//         console.log('es ver moxerxdeba')
+//       }
+// }
+
+// task(11,11)
+
+
+// function number_test(){
+// //    console.log(Math.PI)
+// //    let x = 3.4
+// //    console.log(Math.round(x))
+// //    console.log(Math.floor(x))
+// //    console.log(Math.ceil(x))
+
+// // let t1= 3
+// // let t2= 3.8
+
+// // console.log(parseInt('3.9'))
+
+// // let x = 3
+// // console.log(isNaN(x))
+// }
+
+// number_test()
+
+// function string_test() {
+//     let s = 'hello world'
+//     // console.log(s)
+//     // console.log(s.length)
+//     // console.log(s.toLowerCase())
+
+//     // for(let i= 0; i < s.length; i ++){
+//     //     console.log(s[i])
+//     // }
+
+//     console.log(s.indexOf('world'))
+//     console.log(s.slice(2,7))
+// }
+
+// string_test()
---
+++ leqcia 3/lecture 3/script2.js
@@ -0,0 +1,14 @@
+// function task2(x) {
+//     console.log(Math.ceil(x))
+//     console.log(Math.floor(x))
+//     console.log(Math.round(x))
+//     console.log(Math.random(0,1))
+//     console.log(Math.random(5,50))
+// }
+
+function task(a,b) {
+    const random = Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+// task2(3.2)
+task(2,5)
